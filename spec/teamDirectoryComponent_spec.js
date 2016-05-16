d5415f7516e408587b1af075e123dd4c308b3e26
Narat Suchartsunthorn <me@mixth.net>
2016-05-16 17:19:16 +0700

    Edit description of teamDirectoryComponent_spec to be more specific

--- spec/teamDirectoryComponent_spec.js
+++ spec/teamDirectoryComponent_spec.js
@@ -16,7 +16,7 @@ describe("Team Directory", function() {
     $httpBackend.flush();
   });
 
-  it("should return arrays of with given number of items from list", function() {
+  it("should return array of arrays which consist of 3 or less items each from provided list", function() {
     rawList = [{"value": "1"}, {"value": "2"}, {"value": "3"}, {"value": "4"}, {"value": "5"}, {"value": "6"}, {"value": "7"}];
     result = ctrl.getListInSetOf(rawList, 3);
     expect(result).toEqual(
