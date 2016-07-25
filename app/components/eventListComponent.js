8608eb971574afeafdca95f78017b64637d874ef
Narat S <me@mixth.net>
2016-07-25 13:33:53 +0700

    Change token to SPRINT3R's

--- app/components/eventListComponent.js
+++ app/components/eventListComponent.js
@@ -1,8 +1,8 @@
 (function () {
   function eventListController($http) {
     var self = this;
-    var access_token = "EAAInY6gi9qYBAK5CVMBQOHI5bN36eOvnK19SwPKFfXIy9InRZACTmy12UOGa4FNKvhCU7WuDBwQDeqlRSS1PqNOqPV6Ken2Xv1rBy83Mcy7n1Jow6oIYUKoAjLHQrL1KCjfOaTuhZCbs05Eu2R";
-    var apiEndpoint = "https://graph.facebook.com/v2.6/851344918315730/events?fields=name,start_time,end_time,id,cover,description&access_token=" + access_token;
+    var access_token = "EAAInY6gi9qYBANoakXpLf0DP1JA7thf6pyjJu9APzdVbMakpDzAe06ZCNbh7XyERmbsLXJn2cSKZBOwas6414U3T2jX5eA5Y4ZBuDLLqONKQSedbxZChc4Byng7Y4INVO2UYGZAIh3pZAoFy7SlMK9Pzx9dDWLouYZD";
+    var apiEndpoint = "https://graph.facebook.com/v2.6/203031806488706/events?fields=name,start_time,end_time,id,cover,description&access_token=" + access_token;
 
     self.$onInit = function () {
       $http.get(apiEndpoint, {
